ef0285c5a6d89cface4353962977b62a871e1695
r567tw <r567tw@gmail.com>
2024-05-05 11:10:29 +0800

    fix build pipeline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         items: [
           {
             type: 'doc',
-            docId: 'coding/intro',
+            docId: 'basic/intro',
             position: 'left',
             label: 'Documents',
           },
